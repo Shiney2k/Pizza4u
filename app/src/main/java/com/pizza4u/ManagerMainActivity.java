064c82ab26a430999283bb99f9a734135b155656
sithumini  lasanjana <98685539+sithumini1998@users.noreply.github.com>
2022-10-29 18:10:59 +0530

    Manager (#6)
    * manager form
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Fixed minor issues
    
    * Renamed AddBranchActivity
    
    Co-authored-by: G. Dinusha Sandaruwan <104561942+Dinu5346@users.noreply.github.com>
    Co-authored-by: Dinu5346 <dinusha6326@outlook.com>

---
+++ app/src/main/java/com/pizza4u/ManagerMainActivity.java
@@ -0,0 +1,14 @@
+package com.pizza4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager);
+    }
+}
